8a86ec002ea27d84b6765706c5b733ff57e4ae93
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-06-06 16:23:50 +0545

    navigation drawer :  alternative method

--- app/src/main/java/com/chaudharynabin6/materialdesign/MainActivity.kt
+++ app/src/main/java/com/chaudharynabin6/materialdesign/MainActivity.kt
@@ -1,40 +1,30 @@
 package com.chaudharynabin6.materialdesign
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import android.view.View
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
-
+import androidx.appcompat.app.AppCompatActivity
 import com.chaudharynabin6.materialdesign.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.topAppBar)
 
-        actionBarDrawerToggle = ActionBarDrawerToggle(
-            this,
-            binding.drawerLayout,
-            binding.toolbar,
-            R.string.navDrawerOpen,
-            R.string.navDrawerClose
-        )
 
-        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
+
+        binding.topAppBar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
         binding.navView.setNavigationItemSelectedListener(this)
-
 
 
     }
@@ -42,9 +32,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
         Log.e("menu selected ...",item.toString())
+        item.isCheckable = true
         binding.drawerLayout.close()
         return true
 
     }
+
 
 }
